91e8c1df6519b3477ce243673f7f8797d9ce90e3
Rhavyx <everluca@outlook.com>
2021-08-29 11:13:49 -0300

    :sparkles: plane geometry

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import {Canvas} from '@react-three/fiber';
 
+import './App.css';
+
 const Scene:React.FC = () => {
   return(
     <Canvas>
-
+      <pointLight position={[10,10,10]} />
+      <mesh>
+        <planeBufferGeometry args={[3, 5]} />
+        <meshStandardMaterial color='lightblue' />
+      </mesh>
     </Canvas>
   )
 }
