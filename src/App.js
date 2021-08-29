9fc3767cb0dca01fa511b05025480ec5c0817abc
Rhavyx <everluca@outlook.com>
2021-08-29 13:28:54 -0300

    :art: react shaders wave

--- src/App.js
+++ src/App.js
@@ -1,37 +1,98 @@
-import React from 'react';
-import { shaderMaterial } from '@react-three/drei';
-import glsl from 'babel-plugin-glsl/macro';
-//const glsl = require('babel-plugin-glsl/macro');
-import {Canvas} from '@react-three/fiber';
-
-import './App.css';
+import * as THREE from "three";
+import React, { useRef, Suspense } from "react";
+import { Canvas, extend, useFrame, useLoader } from "@react-three/fiber";
+import { shaderMaterial } from "@react-three/drei";
+import glsl from "babel-plugin-glsl/macro";
+import "./App.css";
 
 const WaveShaderMaterial = shaderMaterial(
-  //Uniform
-  {},
-  //Vertex
-  glsl``,
-  //Fragment Shader
-  glsl``
-)
+  // Uniform
+  {
+    uTime: 0,
+    uColor: new THREE.Color(0.0, 0.0, 0.0),
+    uTexture: new THREE.Texture(),
+  },
+  // Vertex Shader
+  glsl`
+    precision mediump float;
+
+    varying vec2 vUv;
+    varying float vWave;
+
+    uniform float uTime;
+
+    #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
+
+
+    void main() {
+      vUv = uv;
+
+      vec3 pos = position;
+      float noiseFreq = 3.0;
+      float noiseAmp = 0.1;
+      vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
+      pos.z += snoise3(noisePos) * noiseAmp;
+      vWave = pos.z;
+
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);  
+    }
+  `,
+  // Fragment Shader
+  glsl`
+    precision mediump float;
+
+    uniform vec3 uColor;
+    uniform float uTime;
+    uniform sampler2D uTexture;
+
+    varying vec2 vUv;
+    varying float vWave;
+
+    void main() {
+      float wave = vWave * 0.2;
+      vec3 texture = texture2D(uTexture, vUv + wave).rgb;
+      gl_FragColor = vec4(texture, 1.0); 
+    }
+  `
+);
+
+extend({ WaveShaderMaterial });
+
+const Wave = () => {
+  const ref = useRef();
+  useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
+
+  const [image] = useLoader(THREE.TextureLoader, [
+    "./savio.jpg"
+  ]);
+
+  return (
+    <mesh>
+      <planeBufferGeometry args={[0.4, 0.6, 16, 16]} />
+      <waveShaderMaterial uColor={"hotpink"} ref={ref} uTexture={image} />
+    </mesh>
+  );
+};
 
 const Scene = () => {
-  return(
-    <Canvas>
-      <pointLight position={[10,10,10]} />
-      <mesh>
-        <planeBufferGeometry args={[3, 5]} />
-        <meshStandardMaterial color='lightblue' />
-      </mesh>
+  return (
+    <Canvas camera={{ fov: 12, position: [0, 0, 5] }}>
+      <Suspense fallback={null}>
+        <Wave />
+      </Suspense>
     </Canvas>
-  )
-}
+  );
+};
 
 const App = () => {
   return (
-    <Scene />
-  )
-}
-
+    <>
+      <div className="devScene">
+        <Scene  />
+        <h1 className="devName">Everton Sávio</h1>
+      </div>
+    </>
+  );
+};
 
 export default App;
